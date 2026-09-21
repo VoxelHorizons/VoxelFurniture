f83ddc1d3ea54e77d72379c66e1fa7780f2eb287
Dan Alexander <71574715+Fibbicles@users.noreply.github.com>
2026-09-21 20:03:13 +1000

    Revert facing-aware furniture state filters

--- src/main/java/org/voxelhorizons/furniture/model/FurnitureStateSelector.java
+++ src/main/java/org/voxelhorizons/furniture/model/FurnitureStateSelector.java
@@ -6,29 +6,15 @@ public final class FurnitureStateSelector {
     private FurnitureStateSelector() { }
 
     public static Selection select(FurnitureDefinition definition, int neighbors, float baseYaw) {
-        return select(definition, neighbors, neighbors, 0, 0, 0, 0, baseYaw);
+        return select(definition, neighbors, neighbors, baseYaw);
     }
 
     /** Aligned neighbors have the same snapped placement yaw as this furniture. */
     public static Selection select(FurnitureDefinition definition, int neighbors, int alignedNeighbors, float baseYaw) {
-        return select(definition, neighbors, alignedNeighbors, 0, 0, 0, 0, baseYaw);
-    }
-
-    public static Selection select(FurnitureDefinition definition, int neighbors, int alignedNeighbors,
-                                   int perpendicularNeighbors, int clockwiseNeighbors, int counterClockwiseNeighbors,
-                                   int oppositeNeighbors, float baseYaw) {
         FurnitureStateRule chosen = null;
         int turns = -1;
         for (FurnitureStateRule rule : definition.states()) {
-            int sourceNeighbors;
-            switch (rule.neighborFacing()) {
-                case SAME: sourceNeighbors = alignedNeighbors; break;
-                case PERPENDICULAR: sourceNeighbors = perpendicularNeighbors; break;
-                case CLOCKWISE: sourceNeighbors = clockwiseNeighbors; break;
-                case COUNTERCLOCKWISE: sourceNeighbors = counterClockwiseNeighbors; break;
-                case OPPOSITE: sourceNeighbors = oppositeNeighbors; break;
-                default: sourceNeighbors = neighbors;
-            }
+            int sourceNeighbors = rule.relative() && rule.alignedOnly() ? alignedNeighbors : neighbors;
             int localNeighbors = rule.relative()
                     ? FurnitureStateRule.rotate(sourceNeighbors, (4 - (Math.round(baseYaw / 90.0f) & 3)) & 3)
                     : sourceNeighbors;
