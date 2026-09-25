6add237d54bfee83d679e5f0b5bbec0de776e249
Dan Alexander <71574715+Fibbicles@users.noreply.github.com>
2026-09-25 12:29:47 +1000

    Fix multipart parser test assertions

--- src/test/java/org/voxelhorizons/furniture/FurnitureDefinitionParserTest.java
+++ src/test/java/org/voxelhorizons/furniture/FurnitureDefinitionParserTest.java
@@ -17,6 +17,7 @@ import java.util.Map;
 import java.util.Optional;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class FurnitureDefinitionParserTest {
